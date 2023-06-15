ecd59c5d139f910f8c11c7d1053a02e72a30dca9
Bruce <bruce@redravencomputing.com>
2023-06-14 23:24:03 -0500

    Finish with firebase notifications.

--- app/src/main/java/com/example/android/eggtimernotifications/MyFirebaseMessagingService.kt
+++ app/src/main/java/com/example/android/eggtimernotifications/MyFirebaseMessagingService.kt
@@ -1,14 +1,29 @@
 package com.example.android.eggtimernotifications
 
+import android.app.NotificationManager
 import android.util.Log
+import androidx.core.content.ContextCompat
+import com.example.android.eggtimernotifications.util.sendNotification
 import com.google.firebase.messaging.FirebaseMessagingService
 import com.google.firebase.messaging.RemoteMessage
 
 class MyFirebaseMessagingService: FirebaseMessagingService() {
 
 	override fun onMessageReceived(message: RemoteMessage) {
-		//super.onMessageReceived(message)
 		Log.d(TAG, "From: ${message.from}")
+
+		// TODO: Step 3.5 check messages for data
+		// Check if message contains a data payload.
+		message.data.let {
+			Log.d(TAG, "Message data payload: " + message.data)
+		}
+		// TODO: Step 3.6 check messages for notification and call sendNotification
+		// Check if message contains a notification payload.
+		message.notification?.let {
+			Log.d(TAG, "Message Notification Body: ${it.body}")
+			sendNotification(it.body!!)
+		}
+
 	}
 
 	override fun onNewToken(token: String) {
@@ -20,6 +35,15 @@ class MyFirebaseMessagingService: FirebaseMessagingService() {
 		sendRegistrationToServer(token)
 	}
 
+	/**
+	 * Create and show a simple notification containing the received FCM message.
+	 *
+	 * @param messageBody FCM message body received.
+	 */
+	private fun sendNotification(messageBody: String) {
+		val notificationManager = ContextCompat.getSystemService(applicationContext, NotificationManager::class.java) as NotificationManager
+		notificationManager.sendNotification(messageBody, applicationContext)
+	}
 
 	private fun sendRegistrationToServer(token: String?) {
 		// TODO: Implement this method to send token to your app server.
--- app/src/main/java/com/example/android/eggtimernotifications/ui/EggTimerFragment.kt
+++ app/src/main/java/com/example/android/eggtimernotifications/ui/EggTimerFragment.kt
@@ -62,6 +62,7 @@ class EggTimerFragment : Fragment() {
             getString(R.string.breakfast_notification_channel_name)
         )
 
+        subscribeToTopic()
         return binding.root
     }
 
@@ -87,7 +88,20 @@ class EggTimerFragment : Fragment() {
         }
 
         // TODO: Step 1.6 END create a channel
+    }
 
+    private fun subscribeToTopic() {
+        // [START subscribe_topics]
+        FirebaseMessaging.getInstance().subscribeToTopic(TOPIC)
+            .addOnCompleteListener { task ->
+                var msg = getString(R.string.message_subscribed)
+                if (!task.isSuccessful) {
+                    msg = getString(R.string.message_subscribe_failed)
+                }
+                Toast.makeText(context, msg, Toast.LENGTH_SHORT).show()
+
+            }
+        // [END subscribe_topics]
     }
 
     companion object {
